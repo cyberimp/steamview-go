f03d48f3c82a82f8ffe2997ce5aac829ba969f8b
Andrey Kalikin <kinzo.x.beato@gmail.com>
2024-08-15 00:49:38 +0300

    docs on main.go

--- main.go
+++ main.go
@@ -22,12 +22,17 @@ import (
 	"time"
 )
 
+// assets folder packed into binary
+//
 //go:embed assets
 var assets embed.FS
 
+// sumsRaw contains precalculated md5 sums in raw format
+//
 //go:embed md5sums.txt
-var sums string
+var sumsRaw string
 
+// sumsMap contains mapping filename -> md5
 var sumsMap map[string]string
 
 func main() {
@@ -36,8 +41,10 @@ func main() {
 	parseSums()
 	setupHandles()
 
+	//start async parser of Steam DB
 	go appinfo.ParseAsync(path.Join(steam.CacheRoot, "appinfo.vdf"))
 
+	//start worker watching changes in current working appID
 	go worker.Serve()
 
 	httpServer := &http.Server{
@@ -46,18 +53,20 @@ func main() {
 		BaseContext: func(_ net.Listener) context.Context { return ctx },
 	}
 
+	//stop worker on server shutdown, sending "Server is dead" to all clients
 	httpServer.RegisterOnShutdown(func() {
 		worker.Panic()
 	})
 
+	//start http server
 	go func() {
 		if err := httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 			log.Fatalf("HTTP server ListenAndServe: %v", err)
 		}
 	}()
 
+	//setup channel for kill command
 	signalChan := make(chan os.Signal, 1)
-
 	signal.Notify(
 		signalChan,
 		syscall.SIGHUP,  // kill -SIGHUP processID
@@ -65,15 +74,18 @@ func main() {
 		syscall.SIGQUIT, // kill -SIGQUIT processID
 	)
 
+	//run tray icon in separate goroutine
 	quitChan := make(chan struct{})
 	trayicon.Run(quitChan)
 	defer trayicon.Quit()
 
+	//wait for shutdown signal from tray icon or terminal
 	select {
 	case <-signalChan:
 	case <-quitChan:
 	}
 
+	//now shutdown server gracefully
 	gracefulCtx, cancelShutdown := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelShutdown()
 	err := httpServer.Shutdown(gracefulCtx)
@@ -84,18 +96,22 @@ func main() {
 	cancel()
 }
 
+// parseSums parses sumsRaw into sumsMap
 func parseSums() {
-	sumsSlice := strings.Fields(sums)
+	sumsSlice := strings.Fields(sumsRaw)
 	sumsMap = map[string]string{}
 	for i := 0; i < len(sumsSlice); i += 2 {
+		//serving "/" is equal to serving embed.FS/index.html
 		if sumsSlice[i+1] == "index.html" {
 			sumsMap["/"] = sumsSlice[i]
 		}
+		//works both in linux and windows
 		sumsMap["/"+strings.ReplaceAll(sumsSlice[i+1], "\\", "/")] = sumsSlice[i]
 	}
 }
 
-func ServeFSCached(next http.Handler) http.Handler {
+// serveFSCached is middleware for sending embedded resources with their md5sum as e-tag
+func serveFSCached(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "public, max-age=2592000")
 		md5Sum := sumsMap[r.URL.Path]
@@ -104,6 +120,7 @@ func ServeFSCached(next http.Handler) http.Handler {
 	})
 }
 
+// setupHandles sets up server handles
 func setupHandles() {
 	var staticFS = fs.FS(assets)
 	htmlContent, err := fs.Sub(staticFS, "assets")
@@ -111,7 +128,10 @@ func setupHandles() {
 		log.Fatal(err)
 	}
 	ServeFs := http.FileServer(http.FS(htmlContent))
+	//handle for socketserver
 	http.HandleFunc("/socket", wsserver.ServeWs)
+	//handle for steam cache, app will get pictures from real steam cache
 	http.HandleFunc("/cache/", steam.ServeCache)
-	http.Handle("/", ServeFSCached(ServeFs))
+	//any other files would come from assets folder
+	http.Handle("/", serveFSCached(ServeFs))
 }
